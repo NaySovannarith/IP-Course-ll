76e471c3f9af0f993a888ec8c7f872e0d75af301
NaySovannarith <narith2004@gmail.com>
2025-03-09 15:28:34 +0700

    push TP6

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+
+Route::get('/categories', [CategoryController::class, 'getCategories']);
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::controller(CategoryController::class)->prefix('categories')->group(function() {
+    Route::get('/', 'getCategories'); // GET /categories
+    Route::post('/', 'createCategory'); // POST /categories
+    Route::get('{categoryId}', 'getCategory');
+    Route::patch('{categoryId}', 'updateCategory');
+    Route::delete('{categoryId}', 'deleteCategory'); // DELETE /categories/{categoryId}
+
+});
+
+Route::controller(ProductController::class)->prefix('products')->group(function() {
+    Route::get('/', 'getProducts'); // GET /products
+    Route::post('/', 'createProduct'); // POST /products
+    Route::get('{productId}', 'getProduct'); // GET /products/{productId}
+    Route::patch('{productId}', 'updateProduct'); // PATCH /products/{productId}
+    Route::delete('{productId}', 'deleteProduct'); // DELETE /products/{productId}
+});
